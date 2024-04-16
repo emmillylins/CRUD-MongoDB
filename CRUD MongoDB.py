8147a0b690c3dd7b3e4da9fa49ba65777227a8c6
emmy <104332292+emmillylins@users.noreply.github.com>
2024-04-16 15:06:41 -0300

    Update CRUD MongoDB.py

--- CRUD MongoDB.py
+++ CRUD MongoDB.py
@@ -1,68 +1,60 @@
-import pymongo 
-#from pymongo import MongoClientclient = MongoClient()
+import pymongo
+from pymongo import MongoClient
+client = MongoClient()
 
-# Conectar ao MongoDB
+# Conectar ao banco de dados
 client = pymongo.MongoClient('mongodb+srv://teste:teste@cluster0.s8d6d.mongodb.net/sample_training?retryWrites=true&w=majority')
-
 db = client['senac']
 
-# Criar uma coleção para os contatos
-contacts_collection = db['contacts']
+# Definir a coleção
+contacts = db['contacts']
 
 # Função para criar um novo contato
-def create_contact(name, phone_numbers):
-    new_contact = {"name": name, "phone_numbers": phone_numbers}
-    contacts_collection.insert_one(new_contact)
-    print("Contato criado com sucesso!")
+def create_contact(name, number):
+    new_contact = {"name": name, "number": number}   
+    result = contacts.insert_one(new_contact)
+    print("Contato criado: {}".format(result.inserted_id))
 
-# Função para criar vários contatos de uma vez
-def create_multiple_contacts(contacts):
-    contacts_collection.insert_many(contacts)
-    print("Contatos criados com sucesso!")
-
-# Função para ler um contato
-def read_contact(name):
-    contact = contacts_collection.find_one({"name": name})
+# Função para buscar um contato pelo nome
+def find_contact_by_name(name):
+    contact = contacts.find_one({"name": name})
     if contact:
-        print(f"Nome: {contact['name']}, Telefones: {', '.join(contact['phone_numbers'])}")
+        print("Contato encontrado:")
+        print(contact)
     else:
         print("Contato não encontrado.")
 
-# Função para ler todos os contatos
-def read_contacts():
-    contacts = contacts_collection.find()
-    if contacts:
-        for contact in contacts:
-            print(f"Nome: {contact['name']}, Telefones: {', '.join(contact['phone_numbers'])}")
-    else:
-        print("Nenhum contato encontrado.")
+# Função para listar todos os contatos
+def list_contacts():
+    contacts = contacts.find()
+    print("Lista de contatos:")
+    for contact in contacts:
+        print(contact)
 
-# Função para atualizar os telefones de um contato
-def update_contact_phone_numbers(name, new_phone_numbers):
-    contacts_collection.update_one({"name": name}, {"$set": {"phone_numbers": new_phone_numbers}})
-    print("Telefones do contato atualizados com sucesso!")
+# Função para atualizar o telefone de um contato
+def update_contact_number(name, new_number):
+    contacts.update_one({"name": name}, {"$set": {"number": new_number}})
+    print("Telefone do contato atualizado com sucesso!")
 
-# Função para deletar um contato
-def delete_contact(name):
-    contacts_collection.delete_one({"name": name})
-    print("Contato deletado com sucesso!")
+# Função para deletar um contato pelo número
+def delete_contact_by_number(number):
+    contacts.delete_one({"number": number})
+    print("Contato excluído com sucesso!")
 
-# Exemplo de uso
-if __name__ == "__main__":
-    # Criar alguns contatos de exemplo
-    create_contact("João", ["123456789", "987654321"])
-    create_contact("Maria", ["111222333", "444555666"])
+# Exemplos de uso
 
-    # Ler todos os contatos
-    print("\nTodos os contatos:")
-    read_contacts()
+# Criar contatos
+create_contact("João", "123456789")
+create_contact("Maria", "987654321")
 
-    # Atualizar telefones do contato
-    update_contact_phone_numbers("João", ["999999999"])
+# Buscar contato pelo nome
+find_contact_by_name("João")
 
-    # Deletar um contato
-    delete_contact("Maria")
+# Listar todos os contatos
+list_contacts()
 
-    # Ler todos os contatos após as alterações
-    print("\nTodos os contatos após alterações:")
-    read_contacts()
+# Atualizar telefone de um contato
+update_contact_number("Maria", "999999999")
+
+# Deletar um contato pelo nome
+delete_contact_by_number("123456789")
